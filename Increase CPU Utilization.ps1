52da183f0d895bdca83d419e80ff8599da54ace0
vrushabh-dhomane-amla <vrushabh.dhomane@amla.io>
2022-08-05 11:20:58 +0530

    1st

---
+++ Increase CPU Utilization.ps1
@@ -0,0 +1,23 @@
+ForEach ($core in 1..$NumberOfLogicalProcessors){ 
+ 
+start-job -ScriptBlock{
+ 
+    $result = 1;
+    foreach ($loopnumber in 1..2147483647){
+        $result=1;
+        
+        foreach ($loopnumber1 in 1..2147483647){
+        $result=1;
+            
+            foreach($number in 1..2147483647){
+                $result = $result * $number
+            }
+        }
+ 
+            $result
+        }
+    }
+}
+ 
+Read-Host "Press any key to exit..."
+Stop-Job *
